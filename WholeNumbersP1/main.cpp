c3e2eed20f738e451cdf1619bd7608fb27008cc3
876lexii <Alexisford095@gmail.com>
2025-03-15 15:36:21 -0400

    Update main.cpp

--- WholeNumbersP1/main.cpp
+++ WholeNumbersP1/main.cpp
@@ -1,14 +1,37 @@
 #include "Whole_Number.h"
 
+using namespace std;
+
 int main() {
-    WholeNumber num1;  // Should now work correctly
-    cout << "Default constructor value: " << num1.getValue() << endl;
+    // Part 1: Test WholeNumber class
+    WholeNumber::testPart1();
 
-    WholeNumber num2(15);
-    cout << "Parameterized constructor value: " << num2.getValue() << endl;
+    // Part 2: Test constructors
+    WholeNumber::testPart2();
 
-    num2.setValue(25);
-    cout << "Updated value: " << num2.getValue() << endl;
+    // Part 3: Test operator overloading
+    WholeNumber::testPart3();
+
+    // Part 4: Test unary operators
+    WholeNumber::testPart4();
+
+    // Part 5: Test binary operators
+    WholeNumber::testPart5();
+
+    // Part 6: Test comparison operators
+    WholeNumber::testPart6();
+
+    // Part 7: Test assignment operators
+    WholeNumber::testPart7();
+
+    // Part 8: Test increment and decrement operators
+    WholeNumber::testPart8();
+
+    // Part 9: Test [] operator
+    WholeNumber::testPart9();
+
+    // Part 10: Test static functions
+    WholeNumber::testPart10();
 
     return 0;
 }
